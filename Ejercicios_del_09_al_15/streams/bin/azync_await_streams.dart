fbed9f0f74d8e60a9a5117b119c3f173281a1ac9
Arlyule <torresreyessamel2@gmail.com>
2024-07-11 13:34:51 -0600

    Version 1.0

--- Ejercicios_del_09_al_15/streams/bin/azync_await_streams.dart
+++ Ejercicios_del_09_al_15/streams/bin/azync_await_streams.dart
@@ -1,8 +1,7 @@
 //Archivo azync_await_streams.dart
 void main() async {
   Stream<int> stream =
-      Stream.periodic(Duration(seconds: 1), (contador) => contador)
-          .take(5); //Stream periodic
+      Stream.periodic(Duration(seconds: 1), (contador) => contador).take(5);
   await for (var data in stream) {
     print("Data recibida: $data");
   }
